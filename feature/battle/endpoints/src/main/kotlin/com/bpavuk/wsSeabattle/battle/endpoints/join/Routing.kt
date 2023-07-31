02450bb6f5d56385c754cfdbcc067e361735288b
bpavuk <boopentiy@gmail.com>
2023-07-31 10:56:27 +0300

    "Moved room joining logic to JoinRoomPlugin"
    Moved the logic for room joining from the main Routing.kt file into a new JoinRoomPlugin. This change was made to streamline the code and improve readability, adhering to single responsibility principle. Now, all actions related to joining rooms are handled by JoinRoomPlugin, making it easier to modify and test this feature individually.

--- feature/battle/endpoints/src/main/kotlin/com/bpavuk/wsSeabattle/battle/endpoints/join/Routing.kt
+++ feature/battle/endpoints/src/main/kotlin/com/bpavuk/wsSeabattle/battle/endpoints/join/Routing.kt
@@ -1,5 +1,10 @@
 package com.bpavuk.wsSeabattle.battle.endpoints.join
 
+import com.bpavuk.wsSeabattle.core.endpoints.ConnectionContainer
+import com.bpavuk.wsSeabattle.core.endpoints.Plugin
+import com.bpavuk.wsSeabattle.core.types.Connection
+import io.ktor.websocket.*
+
 interface JoinRoomRepository {
     fun join(roomId: Int, userId: Int): JoinRoomResult
 }
@@ -10,4 +15,30 @@ sealed interface JoinRoomResult {
     data object RoomNotFound: JoinRoomResult
 
     data object AlreadyInRoom: JoinRoomResult
+}
+
+class JoinRoomPlugin(
+    private val joinRoomRepository: JoinRoomRepository,
+    allUsers: ConnectionContainer
+): Plugin(allUsers) {
+    override suspend fun onMessage(message: Frame, thisUser: Connection): Boolean {
+        println("join room is called")
+        return if (message is Frame.Text && message.readText().matches("/join ?\\d+?".toRegex())) {
+            val numberFinderRegex = Regex("\\d+")
+            val roomId = numberFinderRegex.find(message.readText())!!.value.toInt()
+            when (
+                joinRoomRepository.join(roomId, thisUser.userId)
+            ) {
+                JoinRoomResult.RoomNotFound ->
+                    thisUser.sendMessage("room with id $roomId not found")
+
+                JoinRoomResult.Success ->
+                    thisUser.sendMessage("joined to room $roomId")
+
+                JoinRoomResult.AlreadyInRoom ->
+                    thisUser.sendMessage("you are already in the room")
+            }
+            true
+        } else false
+    }
 }
--- frontend/src/main/kotlin/com/bpavuk/wsSeabattle/frontend/Routing.kt
+++ frontend/src/main/kotlin/com/bpavuk/wsSeabattle/frontend/Routing.kt
@@ -4,8 +4,8 @@ import com.bpavuk.wsSeabattle.battle.endpoints.create.CreateRoomRepository
 import com.bpavuk.wsSeabattle.battle.endpoints.create.CreateRoomResponse
 import com.bpavuk.wsSeabattle.battle.endpoints.get.GetRoomRepository
 import com.bpavuk.wsSeabattle.battle.endpoints.get.GetRoomResponse
+import com.bpavuk.wsSeabattle.battle.endpoints.join.JoinRoomPlugin
 import com.bpavuk.wsSeabattle.battle.endpoints.join.JoinRoomRepository
-import com.bpavuk.wsSeabattle.battle.endpoints.join.JoinRoomResult
 import com.bpavuk.wsSeabattle.chat.endpoints.ChatRepository
 import com.bpavuk.wsSeabattle.chat.endpoints.ChatResponse
 import com.bpavuk.wsSeabattle.core.endpoints.ConnectionContainer
@@ -17,6 +17,7 @@ import com.bpavuk.wsSeabattle.core.types.toCoordinate
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.launch
 
 fun Route.launchFrontend(dependencies: FrontendDependencies) {
     val connectionContainer = dependencies.connectionContainer
@@ -27,6 +28,7 @@ fun Route.launchFrontend(dependencies: FrontendDependencies) {
                 "/whereami for current room")
 
         dependencies.pluginRegistry.install(QuitPlugin(connectionContainer))
+        dependencies.pluginRegistry.install(JoinRoomPlugin(dependencies.joinRoomRepository, connectionContainer))
 
         for (frame in incoming) {
             connectionContainer.removeInactiveConnections()
@@ -49,20 +51,6 @@ fun Route.launchFrontend(dependencies: FrontendDependencies) {
                                 thisConnection.session.send("you are not joined to any room")
                             is GetRoomResponse.Success ->
                                 thisConnection.session.send("you are in room ${response.room.id}")
-                        }
-                    }
-                    message.matches("/join \\d+".toRegex()) || message.matches("/join ?".toRegex()) -> {
-                        val numberFinderRegex = Regex("\\d+")
-                        val roomId = numberFinderRegex.find(message)!!.value.toInt()
-                        when (
-                            dependencies.joinRoomRepository.join(roomId, thisConnection.userId)
-                        ) {
-                            JoinRoomResult.RoomNotFound ->
-                                thisConnection.session.send("room with id $roomId not found")
-                            JoinRoomResult.Success ->
-                                thisConnection.session.send("joined to room $roomId")
-                            JoinRoomResult.AlreadyInRoom ->
-                                thisConnection.session.send("you are already in the room")
                         }
                     }
                     message.matches("/leave".toRegex()) -> {
@@ -88,8 +76,13 @@ fun Route.launchFrontend(dependencies: FrontendDependencies) {
                     }
                 }
             }
-            dependencies.pluginRegistry.plugins.forEach { plugin ->
-                plugin.onMessage(frame, thisConnection)
+            launch {
+                val pluginIterator = dependencies.pluginRegistry.plugins.iterator()
+                do {
+                    val plugin = pluginIterator.next()
+                    println("plugin ${plugin.javaClass.name} is started")
+                    val isProcessed = plugin.onMessage(frame, thisConnection)
+                } while (!isProcessed && pluginIterator.hasNext())
             }
         }
     }
